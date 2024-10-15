45f686deffe92b1de615379197c8dab1e8b9ff9f
Henrique Queiroz <zeccakut@gmail.com>
2024-10-15 01:34:55 -0300

    Create scripts.js

---
+++ Rocketseat/ex012 Parâmetros das Funções/scripts.js
@@ -0,0 +1,11 @@
+// function expression
+// function anonymous
+const sum = function(number1, number2) {
+	return (number1 + number2)
+}
+
+let number1 = 34, number2 = 25
+
+console.log(`O número 1 é ${number1}`)
+console.log(`O número 2 é ${number2}`)
+console.log(`A soma é ${sum(number1, number2)}`)
