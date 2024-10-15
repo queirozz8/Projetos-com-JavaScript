727abcf0f41f6590e50e34218f2f6d47d54ef02f
Henrique Queiroz <zeccakut@gmail.com>
2024-10-15 01:34:02 -0300

    Create scripts.js

---
+++ Rocketseat/ex011 Funções/scripts.js
@@ -0,0 +1,12 @@
+// Criar um aplicativo de frases motivacionais
+
+function createPhrases() {
+	console.log('Estudar é muito bom')
+	console.log('Estudar é bom demais')
+	console.log('Paciência e persistência')
+	console.log('Revisão é mãe do aprendizado')
+}
+
+// Executar a função
+createPhrases()
+createPhrases()
