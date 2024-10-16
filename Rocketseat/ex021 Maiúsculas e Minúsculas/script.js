357c909fce5cb324e89ca23c5faaac51e5a18797
Henrique Queiroz <zeccakut@gmail.com>
2024-10-16 00:32:57 -0300

    Create script.js

---
+++ Rocketseat/ex021 Maiúsculas e Minúsculas/script.js
@@ -0,0 +1,7 @@
+// Manipulando Strings e Números
+
+// Transforme letras minúsculas em maiúsculas. Faça o contrário disso também
+let word = 'Programar é muito bacana!'
+let word2 = 'PROGRAMAR É MUITO LEGAL!'
+console.log(word.toUpperCase())
+console.log(word2.toLowerCase())
