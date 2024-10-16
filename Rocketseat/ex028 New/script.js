533e62220788768dcd708f5cf77a0a3600fdf087
Henrique Queiroz <zeccakut@gmail.com>
2024-10-16 01:06:30 -0300

    Create script.js

---
+++ Rocketseat/ex028 New/script.js
@@ -0,0 +1,11 @@
+/*
+    new
+
+    * left-hand-side expression
+    * criar um novo objeto
+*/
+
+let nome = new String('Rick')
+nome.surName = 'Queiroz'
+let idade = new Number(23)
+console.log(nome, nome.surName, idade)
