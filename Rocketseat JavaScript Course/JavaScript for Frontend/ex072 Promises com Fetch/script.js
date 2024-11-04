afde9109d58a5d66fb821e324e506b875a8b9958
Henrique Queiroz <zeccakut@gmail.com>
2024-11-04 03:10:32 -0300

    Create script.js

---
+++ Rocketseat JavaScript Course/JavaScript for Frontend/ex072 Promises com Fetch/script.js
@@ -0,0 +1,6 @@
+fetch('https://api.github.com/users/maykbrito')
+.then(response => response.json())
+.then(data => fetch(data.repos_url))
+.then(res => res.json())
+.then(d => console.log(d))
+.catch(err => console.log(err))
