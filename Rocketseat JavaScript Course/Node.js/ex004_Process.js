9c56e4314627dbf8281decc6003cace74b0a0883
Henrique Queiroz <zeccakut@gmail.com>
2024-11-06 01:11:10 -0300

    Create ex004_Process.js

---
+++ Rocketseat JavaScript Course/Node.js/ex004_Process.js
@@ -0,0 +1,7 @@
+/* Para o resultado correto: 
+    - execute esse código com o Node 
+    - passando no final do nome do arquivo o seu nome e sobrenome (não mais do que 2 palavras)
+*/
+const firstName = process.argv[2], lastName = process.argv[3]
+
+console.log(`Seu nome é ${process.argv[2]} ${process.argv[3]}`)
