3afbbe578319d15ee196b517bcdaa072df828d86
Henrique Queiroz <zeccakut@gmail.com>
2024-11-06 07:49:21 -0300

    Update Process.js

--- Rocketseat JavaScript Course/Node.js/ex005 Desafio/Process.js
+++ Rocketseat JavaScript Course/Node.js/ex005 Desafio/Process.js
@@ -1,5 +1,8 @@
 // Arquivo principal
-// Importando a função
+// Quando for executar com o Node, coloque as flags --name "Nome de exemplo" e --greeting "Saudação".
+// A ordem das flags pode ser diferente
+
+
 const getFlagValue = require('./exports')
 // Mesmo que mude a ordem das flags passadas, o output vai ser o mesmo
 console.log(`Olá ${getFlagValue('--name')}, ${getFlagValue('--greeting')}`)
