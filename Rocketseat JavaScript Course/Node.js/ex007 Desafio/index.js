341f71062decccce9a8d81f7edb4172fae11145a
Henrique Queiroz <zeccakut@gmail.com>
2024-11-14 21:37:58 -0300

    Update index.js

--- Rocketseat JavaScript Course/Node.js/ex007 Desafio/index.js
+++ Rocketseat JavaScript Course/Node.js/ex007 Desafio/index.js
@@ -1,25 +1,64 @@
-const ask = (index = 0) => process.stdout.write(questions[index] + ' > ')
-
-
 const questions = [
     'O que aprendi hoje?',
     'O que me deixou aborrecido? E o que eu poderia fazer para melhorar?',
     'O que me deixou feliz hoje?',
     'Quantas pessoas ajudei hoje?',
-]
+];
+let answers = [];
 
-ask()
+const ask = (index = 0) => {
+    if (index < questions.length) {
+        process.stdout.write('\n' + questions[index] + ' > ');
 
-let answers = []
-process.stdin.on('data', data => {
-    answers.push(data.toString().trim())
-    if (answers.length < questions.length) {
-        ask(answers.length)
+        // Remove qualquer listener antigo para evitar duplicação
+        process.stdin.removeAllListeners('data');
+        
+        process.stdin.on('data', data => {
+            let response = data.toString().trim()
+        
+            // Verifica se a pergunta atual é a quarta (índice 3) e espera um número inteiro
+            if (index === 3) {
+                if (!Number.isInteger(Number(response))) {
+                    console.log('\nErro! Por favor, digite um número inteiro para esta questão.')
+                    ask(3)
+                } else {
+                    answers.push(parseInt(response))
+                    ask(answers.length)
+                }
+            } else {
+                answers.push(response)
+                ask(answers.length)
+            }
+        })
     } else {
-        process.stdout.write('\nRESPOSTAS\n\n');
-        for (let answer of answers) {
-            process.stdout.write(questions[answers.indexOf(answer)] + ' ' + answer + '\n')
-        }
-        process.exit()
+        process.stdin.pause(); // Pausa a entrada antes de sair
+        finish() // Exibe as respostas ao terminar as perguntas
     }
-})
+};
+
+// Função para finalizar e exibir as respostas
+const finish = () => {
+    process.stdin.pause(); // Pausa a entrada
+    console.log(`
+    RESPOSTAS
+    O que você aprendeu hoje foi:
+    ${answers[0]}
+    
+    O que te aborreceu e você poderia melhorar foi:
+    ${answers[1]}
+    
+    O que te deixou feliz hoje:
+    ${answers[2]}
+    
+    Você ajudou ${answers[3]} pessoas hoje.
+    
+    
+    Volte amanhã para novas reflexões!
+    `);
+    process.exit(); // Finaliza o processo
+}
+
+
+
+// Começa o processo com a primeira pergunta
+ask();
