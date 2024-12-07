93e729607a6381e9a84936567f1df24fc59f12b0
Henrique Queiroz <zeccakut@gmail.com>
2024-12-07 19:36:49 -0300

    Create server.js

---
+++ Rocketseat JavaScript Course/EJS/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    const items = [
+        { 
+            title: 'D',
+            message: 'esenvolver aplicações/serviços de forma fácil'
+        },
+        {
+            title: 'E',
+            message: 'JS usa JavaScript para renderizar HTML'
+        },
+        { 
+            title: 'M',
+            message: 'uito fácil de usar'
+        },
+        { 
+            title: 'A',
+            message: 'morzinho'
+        },
+        { 
+            title: 'I',
+            message: 'nstall ejs'
+        },
+        { 
+            title: 'S',
+            message: 'intaxe simples'
+        },
+    ]
+
+    const subtitle = 'Uma linguagem de modelagem para criação de páginas HTML utilizando JavaScript'
+    
+    res.render('pages/index', {
+        qualities: items,
+        subtitle: subtitle,
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('pages/about')
+})
+
+app.listen(8080);
+console.log('Servidor funcionando')
